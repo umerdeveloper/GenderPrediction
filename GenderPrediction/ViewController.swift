e7c97a1bf54ff5e7cbb6fb872f112421fbde7bc7
Umer Khan <umar_s66@outlook.com>
2019-10-05 10:17:40 +0500

    Initial Commit

---
+++ GenderPrediction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GenderPrediction
+//
+//  Created by Umer Khan on 05/10/2019.
+//  Copyright © 2019 Umer Khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
