a8b8b1c39c47d5abd78ab81ceeba685bbdcdb605
Umer Khan <umar_s66@outlook.com>
2019-10-05 11:14:41 +0500

    Complete Project

--- GenderPrediction/ViewController.swift
+++ GenderPrediction/ViewController.swift
@@ -1,4 +1,3 @@
-//
 //  ViewController.swift
 //  GenderPrediction
 //
@@ -15,11 +14,45 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         nameTextField.delegate = self
     }
     
+    func predictGenderFromName(_ name: String) -> String? {
+        let featureName = feature(name)
+        
+        let model = GenderByName()
+        if let prediction = try? model.prediction(input: featureName) {
+            if prediction.classLabel == "F" {
+                return "Female"
+            } else { return "Male" }
+        }
+        return nil
+    }
+    
+    // Supporting Function
+    func feature(_ string: String) -> [String: Double] {
+        guard !string.isEmpty else { return [:] }
+        
+        let string = string.lowercased()
+        var keys = [String]()
+        
+        keys.append("FirstLetter1 = (\(string.prefix(1))")
+        keys.append("FirstLetter2 = (\(string.prefix(2))")
+        keys.append("FirstLetter3 = (\(string.prefix(3))")
+        keys.append("FirstLetter1 = (\(string.suffix(1))")
+        keys.append("FirstLetter2 = (\(string.suffix(2))")
+        keys.append("FirstLetter3 = (\(string.suffix(3))")
+        
+        return keys.reduce([String: Double]()) { (result, key) -> [String: Double] in
+            var result = result
+            result[key] = 1.0
+            return result
+        }
+    }
+    
+    // MARK: - TextField Delegate method
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        genderLabel.text = predictGenderFromName(textField.text!)
         return true
     }
 }
